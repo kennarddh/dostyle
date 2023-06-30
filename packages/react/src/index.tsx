ee4a4bf060aa2026f19e4268ba1263e165933e71
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-07-01 01:06:18 +0700

    feat: Basic runtime css in js

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -13,6 +13,9 @@ interface IDefaultProps extends Record<string | number | symbol, unknown> {
 	ref?: Ref<HTMLElementName>
 }
 
+const HypenCashToCamelCase = (str: string) =>
+	str.replace(/-([a-z])/g, (_, up) => up.toUpperCase())
+
 const InterpolationFactory =
 	(element: HTMLElementName) =>
 	<Props extends Omit<IDefaultProps, 'ref'>>(
@@ -36,7 +39,23 @@ const InterpolationFactory =
 
 			const cssString = String.raw({ raw: strings }, ...parsedExpressions)
 
-			console.log({ cssString })
+			const rules = cssString
+				.trim()
+				.split(';')
+				.filter(rule => rule !== '')
+				.map(rule =>
+					rule
+						.trim()
+						.split(':')
+						.map(part => part.trim())
+				)
+
+			const camelCasedRules = rules.map(rule => [
+				HypenCashToCamelCase(rule[0].toLowerCase()),
+				rule[1],
+			])
+
+			const style = Object.fromEntries(camelCasedRules)
 
 			const parsedChildren = Array.isArray(children)
 				? children
@@ -45,7 +64,7 @@ const InterpolationFactory =
 			return createElement(
 				element,
 				{
-					className: 'greeting',
+					style,
 				},
 				...parsedChildren
 			)
