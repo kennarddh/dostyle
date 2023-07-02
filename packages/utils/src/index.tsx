101313bb6eb3974196f23b1f7fe42c72750255ff
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-07-02 17:51:32 +0700

    feat(utils): Random string

--- packages/utils/src/index.tsx
+++ packages/utils/src/index.tsx
@@ -1,2 +1,8 @@
+import crypto from 'crypto'
+
 export const HypenCaseToCamelCase = (str: string) =>
 	str.replace(/-([a-z])/g, (_, up) => up.toUpperCase())
+
+export const RandomString = (length: number) => {
+	return crypto.randomBytes(length).toString('ascii')
+}
