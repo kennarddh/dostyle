0a381a96b01b0704ed79555e2d5229ecacd9b007
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-07-02 21:35:30 +0700

    fix(utils): Use Math.random as random string random generator because it exsist in nodejs and browser

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,9 +1,26 @@
-import crypto from 'crypto'
-
 export const HypenCaseToCamelCase = (str: string) =>
 	str.replace(/-([a-z])/g, (_, up) => up.toUpperCase())
 
-export const RandomString = (length: number) =>
-	crypto.randomBytes(length).toString('ascii')
+export const DecimalToHex = (decimal: number) =>
+	decimal.toString(16).padStart(2, '0')
+
+// https://stackoverflow.com/a/1349426/14813577
+export const RandomString = (length: number) => {
+	const characters =
+		'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+	let counter = 0
+	let result = ''
+
+	while (counter < length) {
+		result += characters.charAt(
+			Math.floor(Math.random() * characters.length)
+		)
+
+		counter += 1
+	}
+
+	return result
+}
 
 export const RandomClassName = () => RandomString(6)
